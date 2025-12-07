ea9ec096d52a33f8fd756d34f1908ae092c68733
Michal Nawilny <mnawilny@gmail.com>
2025-12-07 09:18:10 +0100

    Day 07

--- src/main/kotlin/com/nawilny/aoc2025/day07/Day07.kt
+++ src/main/kotlin/com/nawilny/aoc2025/day07/Day07.kt
@@ -4,5 +4,29 @@ import com.nawilny.aoc2025.commons.Input
 import com.nawilny.aoc2025.commons.Input.println
 
 fun main() {
-    Input.readFileLines("day07", "example.txt").forEach { it.println() }
+    val lines = Input.readFileLines("day07", "input.txt")
+    val splitters = lines.map { line ->
+        line.withIndex().filter { it.value == '^' }.map { it.index }.toSet()
+    }.filter { it.isNotEmpty() }
+    val start = lines.first().indexOf('S')
+    val lineSize = splitters.maxOf { it.max() } + 2
+
+    var possibleTimelines = List(lineSize) { if (it == start) 1L else 0L }
+    var splitterCount = 0
+
+    splitters.forEach { splittersLine ->
+        val newPossibleTimelines = MutableList(lineSize) { 0L }
+        possibleTimelines.withIndex().filter { it.value > 0 }.forEach {
+            if (splittersLine.contains(it.index)) {
+                newPossibleTimelines[it.index-1] += it.value
+                newPossibleTimelines[it.index+1] += it.value
+                splitterCount++
+            } else {
+                newPossibleTimelines[it.index] += it.value
+            }
+        }
+        possibleTimelines = newPossibleTimelines
+    }
+    splitterCount.println()
+    possibleTimelines.sum().println()
 }
